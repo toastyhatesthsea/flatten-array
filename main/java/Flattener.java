c3eb1aa7009b466e0e6e9fd300ffc0142c7bd87b
Saena <toastyhatesthesea@gmail.com>
2018-08-15 04:35:25 -0700

    Completed Flatten Function Using Helper Function CombinedLists

--- main/java/Flattener.java
+++ main/java/Flattener.java
@@ -1,26 +1,18 @@
-import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Flattener
 {
 
-
     public List flatten(List aList)
     {
-        Object[] anArray = aList.toArray();
         List answer = new ArrayList();
 
-
-        for (int i=0; i<anArray.length; i++)
+        for (Object e: aList)
         {
-            Object e = anArray[i];
             if (e instanceof List)
             {
                 List returnedList = flatten((List)e);
-                Collections.addAll(returnedList, answer);
-                //sendBackList(returnedList, answer);
+                combineLists(returnedList, answer);
             } else if (e != null)
             {
                 answer.add(e);
@@ -30,8 +22,12 @@ public class Flattener
         return answer;
     }
 
-
-    public void sendBackList(List aList, List answer)
+    /**
+     * Combine two lists to the second list
+     * @param aList List
+     * @param answer List
+     */
+    public void combineLists(List aList, List answer)
     {
         for (Object e : aList)
         {
@@ -44,26 +40,3 @@ public class Flattener
 }
 
 
-class FlattenTesters
-{
-
-
-    public static void main(String[] argsgsg)
-    {
-        ArrayList aList = new ArrayList();
-
-        aList.add(1);
-        aList.add(null);
-
-        ArrayList numbers = new ArrayList();
-
-
-
-        aList.add(Arrays.asList(numbers));
-
-        Flattener flatten = new Flattener();
-
-        List cleanList = flatten.flatten(aList);
-
-    }
-}
