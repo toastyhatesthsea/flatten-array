f0a59c1325422c70b091ea3486391708c854d9f2
Saena <toastyhatesthesea@gmail.com>
2018-08-14 19:53:44 -0700

    Creating Constructor

---
+++ main/java/Flattener.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Flattener
+{
+
+
+    public List flatten(List aList)
+    {
+        List answer = new ArrayList();
+
+        return answer;
+    }
+
+
+}
+
+
+class FlattenTesters
+{
+
+
+    public static void main(String[] argsgsg)
+    {
+        ArrayList aList = new ArrayList();
+
+        aList.add(1);
+
+        int[] numbers = {4, 5, 6, 7, 8};
+
+        aList.add(Arrays.asList(numbers));
+
+
+
+        //
+
+    }
+}
